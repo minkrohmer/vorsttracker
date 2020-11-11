782e225d0abba02158e0c395b601edfec4e76acc
Mink <minkrohmer@gmail.com>
2020-11-11 15:48:56 +0100

    add .py file

---
+++ vorst.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+
+# +
+def main():
+    r = requests.get('http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam')
+    
+    j = r.json()
+
+    sender        = os.environ['FROM']
+    sendgrid_pass = os.environ['SENDGRID_PASSWORD']
+    sendgrid_user = os.environ['SENDGRID_USERNAME']
+    
+    min_temp_morgen = int(j['liveweer'][0]['d1tmin'])
+    min_temp_overmorgen = int(j['liveweer'][0]['d2tmin'])
+       
+    text = "Hallo, het gaat morgen of overmorgen vriezen. Zet je plantjes binnen! Gr. Mink"
+    
+    if (min_temp_morgen < 1 or min_temp_overmorgen < 1): # Het gaat vriezen
+        import sendgrid
+        sg =sendgrid.SendGridClient(sendgrid_user, sendgrid_pass)
+        message = sendgrid.Mail(subject='Het gaat vriezen! Zet je planten binnen.',
+                               text=text,
+                               from_email = sender)
+    else:
+        return
+    
+    for to in ['minkrohmer@gmail.com', 'vivian_toemen@hotmail.com']:
+        message.add_to(to)
+    
+    status, msg = sg.send(message)
+    print(msg)
+    if status is not 200:
+        sys.exit(1)
+        
+if __name__ == "__main__":
+    main()  
